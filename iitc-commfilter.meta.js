4036e9783e0f76ac8c029a65e77c177e95a7f647
Finntaur <Finntaur@users.noreply.github.com>
2016-09-12 10:49:02 +0300

    Create iitc-commfilter.meta.js
    Initial commit.

---
+++ iitc-commfilter.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-commfilter
+// @name           IITC plugin: COMM Filter
+// @category       Info
+// @version        0.1.0.20160912.095500
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/Finntaur/monkey/raw/master/iitc-commfilter.meta.js
+// @downloadURL    https://github.com/Finntaur/monkey/raw/master/iitc-commfilter.user.js
+// @description    [commfilter-2016-09-12-095500] Filter COMM chat messages to hide unwanted content.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
