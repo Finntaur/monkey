ab5365b8fc1cb82f542179aa38cf75ff1d84fca9
Finntaur <Finntaur@users.noreply.github.com>
2016-12-12 10:34:29 +0200

    Update iitc-commfilter.user.js
    New filter.

--- iitc-commfilter.user.js
+++ iitc-commfilter.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-commfilter
 // @name           IITC plugin: COMM Filter
 // @category       Info
-// @version        0.1.0.20161014.1255
+// @version        0.1.0.20161212.1030
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Finntaur/monkey/raw/master/iitc-commfilter.meta.js
 // @downloadURL    https://github.com/Finntaur/monkey/raw/master/iitc-commfilter.user.js
@@ -32,6 +32,7 @@ window.plugin.commFilter.filters = [
     /^Agent_\d+$/,
     /xmps\.biz/i,
     /ingress\-store\.com/i,
+    /ingress\-shop\.net/i,
     /ingressstore\.ru/i,
     /ingressfarm\.com/i,
     /shop-ingress\.com/i,
