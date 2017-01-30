b86013557a315c1bc2064936da305f3ce04314d4
Finntaur <Finntaur@users.noreply.github.com>
2017-01-30 14:07:25 +0200

    Update iitc-commfilter.meta.js

--- iitc-commfilter.meta.js
+++ iitc-commfilter.meta.js
@@ -2,7 +2,7 @@
 // @id             iitc-commfilter
 // @name           IITC plugin: COMM Filter
 // @category       Info
-// @version        0.1.0.20161212.1030
+// @version        0.1.0.20170130.1405
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/Finntaur/monkey/raw/master/iitc-commfilter.meta.js
 // @downloadURL    https://github.com/Finntaur/monkey/raw/master/iitc-commfilter.user.js
